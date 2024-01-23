6e6635a60f35fe53fa8d1bbabc21169d851f0378
Mike-GH27 <156133464+Mike-GH27@users.noreply.github.com>
2024-01-23 20:01:17 +0100

    Add files via upload

---
+++ init.py
@@ -0,0 +1,11 @@
+"""
+Module structure
+
+- main: starts the application and provides the CLI
+- tracker: provides the habit class, which enables storing habit information and provides methods to change habit
+information
+- databaseSQL: interface between application and database to save user data
+- analytics_module: analyzes habit information, mainly completion data
+- tracker_util: auxiliary functions
+- tracker_testing_data: initializes testing data
+"""
